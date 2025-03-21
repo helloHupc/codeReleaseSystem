f4e1cc7ac7f9e517e48052975c54e95d12833e5c
hupengchen <hupengchen@jiayuan.com>
2025-03-21 18:29:22 +0800

    add config todo

---
+++ pkg/helpers/helpers.go
@@ -0,0 +1,29 @@
+package helpers
+
+import "reflect"
+
+// Empty 类似于 PHP 的 empty() 函数
+func Empty(val interface{}) bool {
+	if val == nil {
+		return true
+	}
+	v := reflect.ValueOf(val)
+	switch v.Kind() {
+	case reflect.String:
+		return v.Len() == 0
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Slice, reflect.Array:
+		return v.Len() == 0
+	case reflect.Map:
+		return v.Len() == 0
+	default:
+		return false
+	}
+}
