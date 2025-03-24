c01096766a0662b7e0650b96c480beb33366c0b1
hupengchen <hupengchen@jiayuan.com>
2025-03-24 16:22:29 +0800

    init db

--- main.go
+++ main.go
@@ -24,6 +24,9 @@ func main() {
 	// 初始化Gin实例
 	router := gin.New()
 
+	// 初始化 DB
+	bootstrap.SetupDB()
+	
 	// 初始化路由绑定
 	bootstrap.SetupRoute(router)
 
---
+++ pkg/database/database.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"gorm.io/gorm"
+	gormlogger "gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+var SQLDB *sql.DB
+
+func Connect(dbConfig gorm.Dialector, _logger gormlogger.Interface) {
+	// 使用 gorm.Open 连接数据库
+	DB, err := gorm.Open(dbConfig, &gorm.Config{
+		Logger: _logger,
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// 将 *gorm.DB 赋值给 SQLDB
+	SQLDB, err = DB.DB()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
