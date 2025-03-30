d2c6459304b8a63fdb5007d886a98921e518da15
hupengchen <hupengchen@rdc.gnway.com>
2025-03-30 23:00:49 +0800

    add mail pkg

--- pkg/mail/mail.go
+++ pkg/mail/mail.go
@@ -1,5 +1,10 @@
 package mail
 
+import (
+	"codeReleaseSystem/pkg/config"
+	"sync"
+)
+
 type From struct {
 	Address string
 	Name    string
@@ -15,6 +20,24 @@ type Email struct {
 	Html    []byte
 }
 
-type Mail struct {
+type Mailer struct {
 	Driver Driver
+}
+
+var once sync.Once
+var internalMailer *Mailer
+
+// NewMailer 单例模式获取
+func NewMailer() *Mailer {
+	once.Do(func() {
+		internalMailer = &Mailer{
+			Driver: &SMTP{},
+		}
+	})
+
+	return internalMailer
+}
+
+func (mailer *Mailer) Send(email Email) bool {
+	return mailer.Driver.Send(email, config.GetStringMapString("mail.smtp"))
 }
